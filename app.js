0a7f8886baa6274b6107031109a16b408a461be0
Igor Girenko <igorg.ideaintech@gmail.com>
2018-08-12 15:16:27 +0300

    Add body-parser, add users view

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const fileUpload = require('express-fileupload');
 const express = require('express');
 const app = express();
-const index = require('./routes/index');
+const app = require('./routes/index');
 const upload = require('./routes/upload');
+const users = require('./routes/users');
 
 app.use(fileUpload());
 
 // routing
-app.use('/', index);
+app.use('/', app);
 app.use('/files', upload);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
